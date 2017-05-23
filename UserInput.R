4f39cdd21769a191f5a9e41e498f6bf7e68c310d
Emanuel Heitlinger <emanuelheitlinger@gmail.com>
2017-05-24 00:06:15 +0200

    Input script still unused and just a junkyard

--- UserInput.R
+++ UserInput.R
@@ -2,10 +2,17 @@
 
 
 ## Get it somehow from the formula
-fo <- loads~ HI*group1*group2
+fo <- loads~ HI + group1*group2
 
 ## the holy grail of how to set this up
 MMat <- model.matrix(object=fo, data=fakedata)
+
+## This will get difficult
+full.MMat <- model.matrix(fo, data=fakedata,
+                          contrasts.arg=
+                              list(group1=diag(nlevels(fakedata$group1)),
+                                   group2=diag(nlevels(fakedata$group2))))
+
 
 ## or we could extract something from here
 terms(fo, data=fakedata)
