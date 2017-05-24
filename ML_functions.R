f21dfc746d9f7e442b9acc4761604475d91f426f
Emanuel Heitlinger <emanuelheitlinger@gmail.com>
2017-05-24 23:53:47 +0200

    LogLikelihood function implementing EH and Alice/Alex seminar changes

--- ML_functions.R
+++ ML_functions.R
@@ -19,44 +19,25 @@ MeanLoad <- function(intercept, growth, alpha, HI){
 }
 
 ## By row
-PrLoadbyRow <- function (data, sub.param) { 
-    PrLoad <- function(data, k=k, alpha=alpha, intercept=intercept, growth=growth){
-        with(data, 
-             ## Always a good idea to make contributions to the likelihood
-             ## function Bombproof, and never return zero
-             max(10^-20, dnbinom(loads, size=abs(k),
-                                 mu=abs(MeanLoad(intercept=intercept,
-                                                 growth=growth,
-                                                 alpha=alpha,
-                                                 HI=HI))))
-             )
-    }
-    sapply(1:nrow(data), function (i){
-        PrLoad(data=data[i, ],
-               k = sub.param[[1]],
-               alpha = sub.param[[2]],
-               intercept = sub.param[[3]],
-               growth = sub.param[[4]])})
-}
-
-#########################
-## Likelihood of a given subgroup of the data (aka male)
-
-## The likelihood function over a set of inds
-## param: k, alpha, mM, mF, slapeM, slapeF
-LikelihoodFunction <- function(data, ugly.param, name) { 
-    ## ugly: get the name back as two strings, but at least it works
-    ## for one factor or many in interactions
-    gname <- strsplit(name, ":")[[1]] 
+LogLik <- function(data, param, group.name){
+    ## split the name into two
+    gname <- strsplit(group.name, ":")[[1]] 
     split.L<- by(data, data[, gname], function(x)  {
-        ## get the name of the paramter from the values within the by
-        ## "loop"
+        ## by makes sure we get all levels: get the name of the paramter
+        ## from the values within the by "loop"
         param.pattern <- unique(interaction(x[, gname], sep=":"))
         ## construct a regex with it
-        par.string <- paste0("^k$|alpha|\\.", param.pattern)
+        par.regex <- paste0("^k$|alpha|\\.", param.pattern)
         ## select from our ugly paramter collection
-        this.param <- ugly.param[grepl(par.string, names(ugly.param))]
-        PrLoadbyRow(x, this.param) # hit and run
-    })
-    sum(log(unlist(split.L)))
+        sub.param <- param[grepl(par.regex, names(param))]
+        sum(dnbinom(x$loads,
+                    size=abs(sub.param[names(sub.param) %in% "k"]),
+                    mu=abs(MeanLoad(alpha=sub.param[names(sub.param) %in% "alpha"],
+                                    intercept=sub.param[grepl("int\\.", names(sub.param))],
+                                    growth=sub.param[grepl("growth\\.", names(sub.param))],
+                                    HI=x$HI)),
+                    log = TRUE), na.rm=TRUE) ## NA removal needed if group has no data
+        })
+        sum(unlist(split.L), na.rm=TRUE) ## NA removal needed if group has no data
 }
+
