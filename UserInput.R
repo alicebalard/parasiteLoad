6bc0bf76cb62b6a0dc84cfc0c46941b2890ce142
Emanuel Heitlinger <emanuelheitlinger@gmail.com>
2017-06-02 22:40:39 +0200

    More checks for input, first output structure

--- UserInput.R
+++ UserInput.R
@@ -13,33 +13,43 @@ glm.hybrid <- function(formula, data,
         stop("glm.hybrid not only currently only implements models with all potential interactions defined")
     } else {
         nb.e <- nb.e[[1]]
-    }
-    var <- nb.e[["variables"]]
-    conti.var <- sapply(var, function (x) !x[["is.factor"]])
-    if (sum(conti.var) != 1 ||
-        !names(conti.var)[conti.var] %in% alpha.along){
-        stop("glm.hybrid is currently only implemented for one continuous variable, which along which a non-linar effect is tested")
-    } else {
-        var.names <- sort(names(nb.e[["variables"]]))
-        original.inverse <- nb.e$transformation$inverse
-        nb.t <- as.data.frame(nb.e, transform = original.inverse)
-        nb.param <- nb.t[, "fit"]
-        names(nb.param) <- apply(nb.t[,var.names], 1,
-                                 paste, collapse=":")
-        nb.param[grepl("\\:1", names(nb.param))] <-
-            nb.param[grepl("\\:1", names(nb.param))] - 
-            nb.param[grepl("\\:0", names(nb.param))]
-        names(nb.param) <- gsub("\\:1", "\\.growth", names(nb.param))
-        names(nb.param) <- gsub("\\:0", "\\.inter", names(nb.param))
-        nb.param <- c(k=nb$theta, alpha=alpha.start, nb.param)
-        optim(par = nb.param,
-              fn = LogLik, ## function to be maximized
-              control = list(fnscale=-1),
-              data = simdata,
-              group.name=names(conti.var[!conti.var]))
+        var <- nb.e[["variables"]]
+        is.factor.var <- sapply(var, function (x) x[["is.factor"]])
+        factor.var <- sort(names(is.factor.var[is.factor.var]))
+        alpha.var <- names(is.factor.var[!is.factor.var])
+        if (length(alpha.var) != 1 ||
+            !all(alpha.var %in% alpha.along) ||
+            max(data[, alpha.var])!=1 ||
+            min(data[, alpha.var])!=0){
+                stop("glm.hybrid is currently only implemented for one continuous variable scaled between 0 and 1, along which a non-linar effect (of intensity alpha) is tested")
+            } else {
+                original.inverse <- nb.e$transformation$inverse
+                nb.t <- as.data.frame(nb.e, transform = original.inverse)
+                nb.param <- nb.t[, "fit"]
+                names(nb.param) <-
+                    apply(nb.t[, c(factor.var, alpha.var)], 1,
+                          paste, collapse=":")
+                ## this works because our maximal value of alpha.along is 1
+                ## get the growth relative to zero
+                nb.param[grepl("\\:1$", names(nb.param))] <-
+                    nb.param[grepl("\\:1$", names(nb.param))] - 
+                    nb.param[grepl("\\:0$", names(nb.param))]
+                ## rename
+                names(nb.param) <- gsub("\\:1$", "\\.growth",
+                                        names(nb.param))
+                names(nb.param) <- gsub("\\:0$", "\\.inter",
+                                        names(nb.param))
+                nb.param <- c(k=nb$theta, alpha=alpha.start, nb.param)
+                opt <- hybrid.maxim(param=nb.param, data=data,
+                                    group.name=factor.var)
+                out <- list(twologlik = opt$value*2,
+                            start.param = nb.param,
+                            start = substitute(start), 
+                            opt.param = opt$par,
+                            df.residual = nb$df.residual-1,
+                            converged = as.logical(opt.para$convergence))
+                class(out) <- append(class(out),"hybrid.glm")
+                return(out)
+            }
     }
 }
-
-
-
-
