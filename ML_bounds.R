4258f69858dfcd5eb851d5e139aac71cac647d7d
Emanuel Heitlinger <emanuelheitlinger@gmail.com>
2017-07-13 13:38:18 +0200

    clean up for merge to master

--- ML_bounds.R
+++ ML_bounds.R
@@ -72,3 +72,112 @@ M
 
 
 
+
+ML_bounds <- function(data, maxL, group.name, opt.param,
+                      threshold = qchisq(p = 0.95, df = 1) / 2){
+  ## maxL given by twologlik/2 from the model
+  myfun <- function(par.name){
+    sinP = opt.param[par.name]
+    allbut1P = opt.param[!opt.param%in%par.name]
+    ## Distance between likelihood and max likelihood:
+    distance.L.ML.oneparfixed <- function(sinP, allbut1P, data, group.name, maxL){
+      L <- LogLik(data = data, param = c(sinP, allbut1P), group.name = group.name)
+      abs(L - (maxL - threshold))
+    }
+    ## Find the parameters (but one) that minimize the distance L - maxL:
+    optim_at_threshold <- function(sinP, allbut1P, data, group.name, maxL){
+      ## consider changing an algorithm that actually works on our problem
+      myOpt <- optim(par = allbut1P,
+                     fn = distance.L.ML.oneparfixed,
+                     sinP = sinP,
+                     data = data,
+                     group.name = group.name,
+                     maxL = maxL)
+      myOpt$par[sinP]
+    }
+    ## Upper bound
+    ## optim instead of optimize to avoid censoring??
+    UB <- optim(f = optim_at_threshold,
+                interval = c(opt.param[[par.name]],
+                             opt.param[[par.name]] + 
+                               abs(opt.param[[par.name]]*100)),
+                maximum=TRUE)
+    ## Lower bounds
+    LB <- optimise(f = optim_at_threshold,
+                   interval = c(opt.param[[par.name]]-
+                                  abs(opt.param[[par.name]]*100),
+                                opt.param[[par.name]]),
+                   maximum=F83;40500;0cALSE)
+    result <- list(c(LB = LB[1], UB = UB[1]))
+    return(result)
+  }
+  ## Run over parameters:
+  mclapply(names(opt.param), myfun, mc.cores=20)
+}
+
+####### TESTS ALICE:
+
+ML_bounds_2 <- function(data, maxL, group.name, opt.param,
+                        threshold = qchisq(p = 0.95, df = 1) / 2){
+  ## maxL given by twologlik/2 from the model
+
+  # set initial parameters:
+  param <- opt.param  
+  
+  # for one given parameter:
+  f1 <- function(param){
+    ## Distance between likelihood and max likelihood:
+      L <- LogLik(data, param, group.name)
+      distance <- abs(L - (maxL - threshold))
+      return(distance)
+  }
+  # optimise the distance, output ONE parameter
+  f2 <- function(param, f1, par.name){
+    opt_f1 <- optim(fn = f1,
+                    par = param,
+                    method = "L-BFGS-B")
+   onepar <- opt_f1$par[par.name]
+   return(onepar)
+  }
+  # optimisation of the optimisation:
+  opt_f2 <- function(f1, f2, par.name, param){
+    optim(fn = f2,
+          par = param,
+          f1 = f1,
+          par.name = par.name)
+  }
+  ## Run over parameters:
+  mclapply("k", opt_f2(f1, f2, "k", param), mc.cores=3)
+}
+
+## FAIL
+#system.time(result <- ML_bounds_2(data = simdata, maxL = glm.h1$twologlik / 2, 
+ #                                 group.name = c("group1", "group2"),
+  #                                opt.param = glm.h1$opt.param, 
+   #                               threshold = qchisq(p = 0.95, df = 1) / 2))
+
+
+### Genius or lame?
+# Wald test (cf "Max Lik estimation and Inference book) p46:
+
+
+# use start values inferred from glm.nb:
+parasite.fit <- optim(par = simpara, 
+                      fn = nLogLik, ## function to be maximized
+                      method = "L-BFGS-B",
+                      data = simdata,
+                      group.name =  c("group1", "group2"),
+                      hessian = TRUE)
+
+MLE <- parasite.fit$par
+ObsInfo <- parasite.fit$hessian # observed Fisher information matrix
+Vhat <- solve(ObsInfo) # inverse of observed Fisher information matrix
+Std.errors <- sqrt(diag(Vhat))
+# obtain the MLEs, estimated std errors, and approx Wald 95% CIs
+Wald.table <- cbind(MLE,
+                    Std.errors,
+                    LowerBounds = MLE - qnorm(0.975)*Std.errors,
+                    UpperBounds = MLE + qnorm(0.975)*Std.errors)
+parnames <- names(opt.para$opt.param)
+rownames(Wald.table) <- parnames
+round(Wald.table, 4)
