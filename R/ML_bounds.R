2d7a441b2b73f88ae27b9931bc8cefcbebdec226
Alice Balard <alice.balard@fu-berlin.de>
2017-07-17 15:20:02 +0200

    ML_bounds BS 97% first implementation

--- R/ML_bounds.R
+++ R/ML_bounds.R
@@ -1,10 +1,21 @@
-### PLEASE DON'T DUPLICATE CODE! TRY it, REALLY!
-### Genius or lame?
+##Approximation of the CI by hessian matrix
 # Wald test (cf "Max Lik estimation and Inference book) p46:
+nloglik <- function(data, param, group.name){
+  -LogLik(data, param, group.name)
+}
+
+hybrid.minim <- function (param, data, group.name, hessian=FALSE){
+  optim(par = param, 
+        fn = nloglik, ## function to be maximized
+        method = "L-BFGS-B",
+        data = data,
+        group.name = group.name,
+        hessian = hessian)
+}
 
 ML_bounds_Wald <- function(param, data, group.name){
   # use start values inferred from glm.nb:
-  fit.include.hessian <- hybrid.maxim(param, data, group.name, hessian=TRUE)
+  fit.include.hessian <- hybrid.minim(param, data, group.name, hessian=TRUE)
   MLE <- fit.include.hessian$par
   ObsInfo <- fit.include.hessian$hessian # observed Fisher information matrix
   Vhat <- solve(ObsInfo) # inverse of observed Fisher information matrix
@@ -15,178 +26,4 @@ ML_bounds_Wald <- function(param, data, group.name){
                       LowerBounds = MLE - qnorm(0.975)*Std.errors,
                       UpperBounds = MLE + qnorm(0.975)*Std.errors)
   round(Wald.table, 4)
-  
-  Vhat
 }
-
-ML_bounds_Wald(param = simpara, data = simdata,
-               group.name =  c("group1", "group2"))
-  
-#parnames <- names(opt.para$opt.param)
-#rownames(Wald.table) <- parnames
-
-
-
-
-
-
-######################################## JUNK
-
-##Calculate upper and lower bounds of each parameters
-ML_bounds <- function(data, group.name, startpara, threshold){
-    maxL <- optim(par = startpara,
-                  fn = LogLik, ## function to be maximized
-                  control = list(fnscale=-1),
-                  data = data,
-                  group.name = group.name)
-    ## Define the function for ONE parameter:
-    myfun <- function(i){
-        sinP = startpara[i]
-        allbut1P = startpara[-i]
-        ## Distance between likelihood and max likelihood:
-        distance.L.ML.oneparfixed <- function(sinP, allbut1P, i, data, group.name, maxL){
-            L <- LogLik(data = data, param = c(sinP, allbut1P), group.name = group.name)
-            abs(L - (maxL$value - threshold))
-        }
-        ## Find the parameters (but one) that minimize the distance L - maxL:
-        Singlepar_ML_bounds <- function(sinP, allbut1P, i, data, group.name, maxL){
-            myOpt <- optim(par = allbut1P,
-                           fn = distance.L.ML.oneparfixed,
-                           sinP = sinP,
-                           i= i,
-                           data = data,
-                           group.name = group.name,
-                           maxL = maxL)
-            myOpt$value
-        }
-        ## For one chosen parameter (rank i)
-        chosenP <- function(sinP, i){
-            names(sinP) <- names(startpara)[i]
-            Singlepar_ML_bounds(sinP = sinP,
-                                allbut1P = allbut1P,
-                                i = i,
-                                data = data,
-                                group.name = group.name,
-                                maxL = maxL)
-        }
-        ## Upper bound
-        UB <- optimise(f = chosenP, i = i, interval = c(maxL$par[[i]], maxL$par[[i]] + 10))
-        ## Lower bounds
-        LB <- optimise(f = chosenP, i = i, interval = c(maxL$par[[i]] -10, maxL$par[[i]]))
-        result <- list(c(LB = LB[1], est = maxL$par[[i]], UB = UB[1]))
-        names(result) <- names(startpara[i])
-        return(result)
-    }
-    ## Run over parameters:
-    sapply(1:length(startpara), myfun)
-}
-
-
-## Example (NB takes +/- 15min to run):
-source("Simulate_and_test.R")
-
-AliceTest <- ML_bounds(data = simdata,
-                       group.name = "group1:group2",
-                       startpara = simpara,
-                       threshold = qchisq(p = 0.95, df = 1) / 2)
-
-## Compare with the actual parameters used for simulation:
-M <- lapply(1:length(simpara), function(i) {
-    M <- data.frame(c(simpara[i], unlist(AliceTest[i])))
-    names(M) <- names(simpara[i])
-    M
-})
-
-M <- as.data.frame(M)
-rownames(M) <- c("simu", "LB", "est", "UB")
-M
-
-
-
-
-
-ML_bounds <- function(data, maxL, group.name, opt.param,
-                      threshold = qchisq(p = 0.95, df = 1) / 2){
-  ## maxL given by twologlik/2 from the model
-  myfun <- function(par.name){
-    sinP = opt.param[par.name]
-    allbut1P = opt.param[!opt.param%in%par.name]
-    ## Distance between likelihood and max likelihood:
-    distance.L.ML.oneparfixed <- function(sinP, allbut1P, data, group.name, maxL){
-      L <- LogLik(data = data, param = c(sinP, allbut1P), group.name = group.name)
-      abs(L - (maxL - threshold))
-    }
-    ## Find the parameters (but one) that minimize the distance L - maxL:
-    optim_at_threshold <- function(sinP, allbut1P, data, group.name, maxL){
-      ## consider changing an algorithm that actually works on our problem
-      myOpt <- optim(par = allbut1P,
-                     fn = distance.L.ML.oneparfixed,
-                     sinP = sinP,
-                     data = data,
-                     group.name = group.name,
-                     maxL = maxL)
-      myOpt$par[sinP]
-    }
-    ## Upper bound
-    ## optim instead of optimize to avoid censoring??
-    UB <- optim(f = optim_at_threshold,
-                interval = c(opt.param[[par.name]],
-                             opt.param[[par.name]] + 
-                               abs(opt.param[[par.name]]*100)),
-                maximum=TRUE)
-    ## Lower bounds
-    LB <- optimise(f = optim_at_threshold,
-                   interval = c(opt.param[[par.name]]-
-                                  abs(opt.param[[par.name]]*100),
-                                opt.param[[par.name]]),
-                   maximum=FALSE)
-    result <- list(c(LB = LB[1], UB = UB[1]))
-    return(result)
-  }
-  ## Run over parameters:
-  mclapply(names(opt.param), myfun, mc.cores=20)
-}
-
-####### TESTS ALICE:
-
-ML_bounds_2 <- function(data, maxL, group.name, opt.param,
-                        threshold = qchisq(p = 0.95, df = 1) / 2){
-  ## maxL given by twologlik/2 from the model
-
-  # set initial parameters:
-  param <- opt.param  
-  
-  # for one given parameter:
-  f1 <- function(param){
-    ## Distance between likelihood and max likelihood:
-      L <- LogLik(data, param, group.name)
-      distance <- abs(L - (maxL - threshold))
-      return(distance)
-  }
-  # optimise the distance, output ONE parameter
-  f2 <- function(param, f1, par.name){
-    opt_f1 <- optim(fn = f1,
-                    par = param,
-                    method = "L-BFGS-B")
-   onepar <- opt_f1$par[par.name]
-   return(onepar)
-  }
-  # optimisation of the optimisation:
-  opt_f2 <- function(f1, f2, par.name, param){
-    optim(fn = f2,
-          par = param,
-          f1 = f1,
-          par.name = par.name)
-  }
-  ## Run over parameters:
-  mclapply("k", opt_f2(f1, f2, "k", param), mc.cores=3)
-}
-
-## FAIL
-#system.time(result <- ML_bounds_2(data = simdata, maxL = glm.h1$twologlik / 2, 
- #                                 group.name = c("group1", "group2"),
-  #                                opt.param = glm.h1$opt.param, 
-   #                               threshold = qchisq(p = 0.95, df = 1) / 2))
-
-
-
--- tests/Simulate_and_test.R
+++ tests/Simulate_and_test.R
@@ -1,4 +1,7 @@
 ### source or load package once done with packaging
+library(devtools)
+install_github("alicebalard/Parasite_Load")
+
 simpara <- c(k = 2, alpha = 1.92,
              "male:old.inter" = 14,
              "male:young.inter" = 12,
@@ -17,23 +20,23 @@ simpara <- c(k = 2, alpha = 1.92,
 
 #########################    
 SimulatedData <- function(param, n){
-    gdata <- data.frame(group1 = rep(c("male", "female"), each=n/2),
-                        group2 = sample(c("old", "young", "baby"),
-                                        n, replace=TRUE))
-    gdata$HI<- round(runif(n), 2)
-    xloads <- by(gdata, gdata$group1:gdata$group2, function (x) {
-        pattern <- paste0("^", unique(x$group1), ":", unique(x$group2))
-        this.param <- param[grepl(pattern, names(param))]
-        loads <- rnbinom(n = nrow(x), size = param["k"],
-                         mu = glm.hybrid:::MeanLoad(intercept=this.param[grepl("\\.inter",
-                                                                               names(this.param))],
-                                                    growth=this.param[grepl("\\.growth",
-                                                                            names(this.param))],
-                                                    alpha=param["alpha"],
-                                                    HI=x$HI))
-        cbind(x, loads)
-    })
-    as.data.frame(do.call("rbind", xloads))
+  gdata <- data.frame(group1 = rep(c("male", "female"), each=n/2),
+                      group2 = sample(c("old", "young", "baby"),
+                                      n, replace=TRUE))
+  gdata$HI<- round(runif(n), 2)
+  xloads <- by(gdata, gdata$group1:gdata$group2, function (x) {
+    pattern <- paste0("^", unique(x$group1), ":", unique(x$group2))
+    this.param <- param[grepl(pattern, names(param))]
+    loads <- rnbinom(n = nrow(x), size = param["k"],
+                     mu = glm.hybrid:::MeanLoad(intercept=this.param[grepl("\\.inter",
+                                                                           names(this.param))],
+                                                growth=this.param[grepl("\\.growth",
+                                                                        names(this.param))],
+                                                alpha=param["alpha"],
+                                                HI=x$HI))
+    cbind(x, loads)
+  })
+  as.data.frame(do.call("rbind", xloads))
 }
 
 set.seed(5)
@@ -89,4 +92,53 @@ glm.hybrid::glm.hybrid(formula=loads~group1*HI, data=simdata, "HI")$twologlik/2
 NBglm <- glm.hybrid::glm.nb(formula=loads~group1*group2*HI, data=simdata)
 NBglm$twologlik/2
 
+######################################################################################
+## ML_bounds tests:
+source("ML_functions.R")
+source("ML_bounds.R")
 
+# Generate random simdata:  (based on Phoung's oocysts counting)
+simdata_generator <- function(){
+  I <- round(runif(6, 0, 4), 2)
+  S <- round(runif(6, -I, 1), 2)
+  simparaBS <- c(k = round(abs(runif(1, 1, 8)), 2),
+                 alpha = round(runif(1, -3, 3), 2),
+                 "male:old.inter" = I[1],
+                 "male:young.inter" = I[2],
+                 "male:baby.inter" = I[3],
+                 "female:old.inter" = I[4],
+                 "female:young.inter" = I[5],
+                 "female:baby.inter" = I[6],
+                 "male:old.growth" = S[1],
+                 "male:young.growth" = S[2],
+                 "male:baby.growth" = S[3],
+                 "female:old.growth" = S[4],
+                 "female:young.growth" = S[5],
+                 "female:baby.growth" = S[6])
+  simdataBS <- SimulatedData(simparaBS, 1000)
+  # output:
+  list(simparaBS, simdataBS)
+}
+
+# Bootstrap the CI:
+myBS <- function(){
+  simBS <- simdata_generator()
+  simpara <- simBS[[1]]
+  simdata <- simBS[[2]]
+  # Calculate bounds 95%CI:
+  bounds <- ML_bounds_Wald(param = simpara, data = simdata,
+                           group.name =  c("group1", "group2"))
+    # Check if the simpara are indeed in the good CI:
+  bounds <- as.data.frame(bounds)
+  bounds$actualpara <- simpara
+  (sum(bounds$LowerBounds <= bounds$actualpara) + sum(bounds$UpperBounds >= bounds$actualpara)) /28*100
+}
+
+# Give NA instead of error:
+myBS_secured <- function(){
+  tryCatch(myBS(), error=function(err) NA)
+} 
+
+# Big BS:
+result <- replicate(100, myBS_secured())
+mean(na.omit(result))
