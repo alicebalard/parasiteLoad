e9df4b0804eb94f7b267f5c770551a8eec8e22f4
Alice Balard <alice.balard@fu-berlin.de>
2018-09-15 18:10:00 +0200

    plot correct banana DONE! YIPEAH!

--- R/Models/trytoPlot.R
+++ R/Models/trytoPlot.R
@@ -43,97 +43,55 @@ cols = c("red", "blue")
       print("Fitting expected response for H0")
       # data$response <- data[[response]] # little trick
       
-      toOptimise <- function(v){
+      expectedResponse <- function(v, hybridIndex){
         L1 = v[1]; L2 = v[2]; alpha = v[3]
         heterozygoty <- 2 * hybridIndex * (1 - hybridIndex)
-        mean <- (L1 + (L2 - L1) * hybridIndex) * (1 - alpha * heterozygoty)
-        return(mean)
+        expectedResponse <- (L1 + (L2 - L1) * hybridIndex) * (1 - alpha * heterozygoty)
+        return(expectedResponse)
       }
-      
-      fitted <- function(HI){
-        hybridIndex = HI
-        toOptimise(v = c(coef(fit$H0)["L1"], coef(fit$H0)["L1"], coef(fit$H0)["alpha"]))
-      }
-      
-      fitted(3)
-      
-      
-      # Run over HI values
+      #test
+      expectedResponse(c(1,2,.3), .4)
+      # Run over HI values and optimise max and min for each
       bananaDF = data.frame(HI = NA, min = NA, max = NA)
       for(hybridIndex in seq(0,1, 0.1)){
         maxLoad <- optim(par = c(L1 = getMax("L1"),
                                  L2 = getMax("L1"),
                                  alpha = mean(getMax("alpha") - getMin("alpha"))),
-                         fn = toOptimise,
+                         fn = expectedResponse,
                          lower = c(L1 = getInf("L1"), L2 = getInf("L1"), alpha = getInf("alpha")),
                          upper = c(L1 = getSup("L1"), L2 = getSup("L1"), alpha = getSup("alpha")),
                          method = "L-BFGS-B",
-                         control = list(fnscale=-1)) # maximize
+                         control = list(fnscale=-1),
+                         hybridIndex = hybridIndex) # maximize
         minLoad <- optim(par = c(L1 = getMax("L1"),
                                  L2 = getMax("L1"),
                                  alpha = mean(getMax("alpha") - getMin("alpha"))),
-                         fn = toOptimise,
+                         fn = expectedResponse,
                          lower = c(L1 = getInf("L1"), L2 = getInf("L1"), alpha = getInf("alpha")),
                          upper = c(L1 = getSup("L1"), L2 = getSup("L1"), alpha = getSup("alpha")),
-                         method = "L-BFGS-B")
+                         method = "L-BFGS-B",
+                         hybridIndex = hybridIndex)
         bananaDF = rbind(bananaDF, data.frame(HI = hybridIndex, min = minLoad$value, max = maxLoad$value))
       }
-      
-      fitDF <- data.frame(HI = seq(0, 1, 0.1), 
-                          fit = toOptimise(v = c(coef(fit$H0)["L1"], coef(fit$H0)["L1"], coef(fit$H0)["alpha"])))
-
-      
-      fitted(0.2)
-      
-      
+      bananaDF <- bananaDF[-1,]
+      bananaDF$fit <- expectedResponse(v = c(coef(fit$H0)["L1"], coef(fit$H0)["L1"], coef(fit$H0)["alpha"]),
+                                       hybridIndex = seq(0,1,.1))
       # Draw the line for the parameters at their MLE, alpha varying 
       ggplot() + 
-        geom_point(data = data, aes_string(x = "HI", y = "response", color = mygroup)) + 
-        scale_color_manual(values = cols) +
+        geom_point(data = data, aes_string(x = "HI", y = "response", fill = mygroup), pch = 21, size = 3) + 
+        scale_fill_manual(values = cols) +
         geom_ribbon(aes(x = bananaDF$HI,
                         ymin = bananaDF$min,
                         ymax = bananaDF$max),
-                    fill = "grey", alpha = .5) #+
-      
-      
-      
-        geom_line(aes(x = bananaDF$HI, y = log10(DF$loadMLE + 1))) +
-        theme_bw(base_size = 20)+
-        ylab(label = labelfory)
-    } else {
-      ggplot() + 
-        geom_point(data = data, aes_string(x = "HI", y = "response", color = mygroup)) + 
-        scale_color_manual(values = cols) +
-        geom_ribbon(aes(x = DF$HI,
-                        ymin = DF$loadMLEAlphaUB,
-                        ymax = DF$loadMLEAlphaLB),
                     fill = "grey", alpha = .5) +
-        geom_line(aes(x = DF$HI, y = DF$loadMLE)) +
-        theme_bw(base_size = 20)+
-        ylab(label = labelfory)
-    }
-  } else {
-    ## Draw the line for the parameters at their MLE, alpha varying 
-    DF <- data.frame(HI = seq(0,1,0.01), 
-                     loadMLE = MeanLoad(L1 = coef(mod)[names(coef(mod)) == "L1"], 
-                                        L2 =  coef(mod)[names(coef(mod)) == "L2"], 
-                                        alpha =  coef(mod)[names(coef(mod)) == "alpha"],  
-                                        hybridIndex = seq(0,1,0.01)))
-    # Do we plot on log10 scale?
-    if(isLog10 == TRUE){
-      ggplot() + 
-        geom_point(data = data, aes_string(x = "HI", y = "log10resp", color = mygroup)) + 
-        scale_color_manual(values = cols) +
-        geom_line(aes(x = DF$HI, y = log10(DF$loadMLE + 1))) +
-        theme_bw(base_size = 20)+
-        ylab(label = labelfory)
-    } else {
-      ggplot() + 
-        geom_point(data = data, aes_string(x = "HI", y = "response", color = mygroup)) + 
-        scale_color_manual(values = cols) +
-        geom_line(aes(x = DF$HI, y = DF$loadMLE)) +
-        theme_bw(base_size = 20)+
-        ylab(label = labelfory)
-    }
-  }
-}
+        geom_line(aes(x = bananaDF$HI, y = bananaDF$fit)) +
+        theme_bw(base_size = 20) +
+        ylab(label = labelfory) +
+        geom_text(aes(.5, bananaDF$fit[bananaDF$HI == .5] +1,
+                      label = paste("alpha = ", round(coef(fit$H0)["alpha"], 2)))) 
+    
+      
+      
+      
+      
+      
