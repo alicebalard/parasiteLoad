d16536125e46cd555f2a050c0fb1aea02b3b029a
Alice Balard <alice.cam.balard@gmail.com>
2019-09-14 16:49:22 +0200

    add print, lrt.mlHyb

--- R/mlHyb.R
+++ R/mlHyb.R
@@ -10,10 +10,12 @@
 #' @param hybridEffect Logical. TRUE if hybrid effect (deviation from additivity between both parents) is tested, FALSE if not.
 #' @param config
 #' @return mlHyb returns an object of class "mlHyb"
-#' The functions summary and anova are used to obtain and print a summary and analysis of variance table of the results. The generic accessor functions coefficients, effects, fitted.values and residuals extract various useful features of the value returned by lm.
+#' The function print creates a user friendly output
+#' The functions lrt.mlHyb is used to obtain a likelihood ratio test (G-test) between models with and without bend (hybridization effect)
 #' An object of class "mlHyb" is a list containing at least the following components:
-#'   coefficients
-#' TO BE UPDATED
+#'   fit : the ML fit considering hybridization effect
+#'   fitNull : the ML fit considering perfect additivity between both sides (null hypothesis on hybridization effect)
+#'   call : the call
 #' @export
 
 mlHyb <- function(formula, data, model, hybridIndex = "HI", myparamBounds = "default",
@@ -44,23 +46,24 @@ mlHyb <- function(formula, data, model, hybridIndex = "HI", myparamBounds = "def
   } else {
     paramBounds <- myparamBounds
   }
-  # is the hybrid effect tested?
-  if (hybridEffect == FALSE){
-    fit <- fit0(data, response, hybridIndex, paramBounds, config)
-  } else {
-    fit <- fit1(data, response, hybridIndex, paramBounds, config)
-  }
+  # test with or without hybrid effect
+  fitNull <- fit0(data, response, hybridIndex, paramBounds, config)
+  fitHE <- fit1(data, response, hybridIndex, paramBounds, config)
   # create an S3 object in list
-  out <- list(fit = fit, call = match.call())
+  out <- list(fit = fitHE, fitNull = fitNull, call = match.call())
   class(out) <- append(class(out), "mlHyb")
   return(out)
 }
 
 print.mlHyb <- function(x){
-  coefShort <- list(intercept = x$fit@coef[["L1"]], slope = x$fit@coef[["L2"]] - x$fit@coef[["L1"]])
   if (x$call$hybridEffect == TRUE){
-    coefShort <- c(coefShort, bend = x$fit@coef[["alpha"]])}
-  coefDetailed <- x$fit@coef
+    coefShort <- list(intercept = x$fit@coef[["L1"]], slope = x$fit@coef[["L2"]] - x$fit@coef[["L1"]],
+                      bend = x$fit@coef[["alpha"]])
+    coefDetailed <- x$fit@coef
+  } else {
+    coefShort <- list(intercept = x$fitNull@coef[["L1"]], slope = x$fitNull@coef[["L2"]] - x$fitNull@coef[["L1"]])
+    coefDetailed <- x$fitNull@coef
+  }
   cat("\nCall:\n",
       paste(deparse(x$call), sep = "\n", collapse = "\n"), "\n\n", sep = "")
   cat("Coefficients:\n")
@@ -69,3 +72,8 @@ print.mlHyb <- function(x){
   print.default(format(coefDetailed, digits = 3L), print.gap = 2L, quote = FALSE)
 }
 
+# make it look like summary(lm(Aspiculuris_Syphacia~Sex, data = toyData))
+lrt.mlHyb <- function(x){
+  cat("Likelihood ratio test (G-test) for significance of bend (hybridization effect):\n\n")
+  Gtest(x$fit, x$fitNull)
+}
--- toputinvignette.R
+++ toputinvignette.R
@@ -4,6 +4,21 @@ toyData <-toyData[toyData$Aspiculuris_Syphacia != 0,]
 m <- mlHyb(Aspiculuris_Syphacia~., data = toyData, model = "negbin", hybridEffect = TRUE)
 m
 
-m0 <- mlHyb(Aspiculuris_Syphacia~., data = toyData, model = "negbin",
-                          hybridEffect = FALSE)
+m0 <- mlHyb(Aspiculuris_Syphacia~., data = toyData, model = "negbin", hybridEffect = FALSE)
 m0
+
+lrt.mlHyb(m)
+##### to do
+
+# groups
+mlHyb(Aspiculuris_Syphacia~Sex, data = toyData, model = "negbin", hybridEffect = TRUE)
+mlHyb(Aspiculuris_Syphacia~., data = toyData, model = "negbin", hybridEffect = TRUE)
+
+# fix sides?
+
+# print table 4 hypothesis
+
+# plots
+# plot(lm(Aspiculuris_Syphacia~Sex, data = toyData)) Ca ira
+
+
